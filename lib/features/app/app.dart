2334e2218739c597a7417c435c271ff90a6b5785
Andrii Kovalchuk <tieorange@gmail.com>
2024-01-24 23:52:22 +0200

    App class setup

---
+++ lib/features/app/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_flutter/core/l10n/l10n.dart';
+import 'package:weather_app_flutter/features/home_page/presentation/view/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const HomePage(),
+    );
+  }
+}

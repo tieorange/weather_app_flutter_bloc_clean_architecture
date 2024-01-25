faf27e96bf00dc7c98d58af44f5d0722bff7d069
Andrii Kovalchuk <tieorange@gmail.com>
2024-01-25 23:04:06 +0200

    Added intro animation

--- lib/features/splash_page/presentation/view/splash_screen.dart
+++ lib/features/splash_page/presentation/view/splash_screen.dart
@@ -1,12 +1,45 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:rive/rive.dart';
+import 'package:weather_app_flutter/core/generated/assets.gen.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Placeholder());
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: _buildAnimations(height, width) /*buildRiveAnimation()*/,
+    );
+  }
+
+  Column _buildAnimations(double height, double width) {
+    return Column(
+      children: [
+        SizedBox(
+          height: height / 1.2,
+          width: width,
+          child: LottieBuilder.asset(Assets.animations.rain),
+        ),
+        Text(
+          'Weather app',
+          style: TextStyle(color: Colors.white70, fontSize: 30),
+        ),
+      ],
+    );
+  }
+
+  RiveAnimation buildRiveAnimation() {
+    return RiveAnimation.asset(
+      Assets.animations.balloonist,
+      // 'https://cdn.rive.app/animations/vehicles.riv',
+    );
   }
 }

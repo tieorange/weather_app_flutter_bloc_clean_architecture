2289a28c87bc014707072f0166a0285a33eaa607
Andrii Kovalchuk <tieorange@gmail.com>
2024-01-26 21:53:40 +0200

    Fixed tests

--- test/features/app/view/app_test.dart
+++ test/features/app/view/app_test.dart
@@ -1,12 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:weather_app_flutter/features/app/app.dart';
-import 'package:weather_app_flutter/features/home_page/presentation/view/home_page.dart';
+import 'package:weather_app_flutter/features/home_page/presentation/bloc/home_cubit.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders App', (tester) async {
+  testWidgets('App widget test', (WidgetTester tester) async {
+    // Wrap your test code with FakeAsync
+    await tester.runAsync(() async {
+      // Build our app and trigger a frame.
       await tester.pumpWidget(const App());
-      expect(find.byType(HomePage), findsOneWidget);
+
+      // You may need to pump frames to complete any pending async operations
+      await tester.pump();
+
+      // Your test assertions go here
+      expect(find.byType(MaterialApp), findsOneWidget);
+      expect(find.byType(BlocProvider<HomePageCubit>), findsOneWidget);
     });
   });
 }
--- test/features/home_page/presentation/bloc/home_page_bloc_test.dart
+++ test/features/home_page/presentation/bloc/home_page_bloc_test.dart
@@ -1,33 +1,120 @@
-/*
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:weather_app_flutter/core/error/failures.dart';
+import 'package:weather_app_flutter/features/home_page/domain/usecase/get_city_by_name_usecase.dart';
+import 'package:weather_app_flutter/features/home_page/domain/usecase/get_weather_data_usecase.dart';
 import 'package:weather_app_flutter/features/home_page/presentation/bloc/home_cubit.dart';
 
+import 'dummyDto.dart';
+import 'home_page_bloc_test.mocks.dart';
+
+// // class MockGetWeatherDataUseCase extends Mock implements GetWeatherDataUseCase {}
+// class MockGetCityByNameUseCase extends Mock implements GetCityByNameUseCase {}
+
+@GenerateNiceMocks([
+  MockSpec<GetWeatherDataUseCase>(),
+  MockSpec<GetCityByNameUseCase>(),
+])
 void main() {
+  late MockGetWeatherDataUseCase mockWeatherUseCase;
+  late MockGetCityByNameUseCase mockCityByNameUseCase;
+
+  setUp(() {
+    mockWeatherUseCase = MockGetWeatherDataUseCase();
+    mockCityByNameUseCase = MockGetCityByNameUseCase();
+  });
+
   group('HomePageCubit', () {
-    group('constructor', () {
-      test('can be instantiated', () {
-        expect(
-          HomePageCubit(),
-          isNotNull,
+    const lat = 52.0;
+    const lon = 56.0;
+    const units = 'metric';
+    final weatherDto = DummyData.getDummyWeatherDto();
+
+    blocTest(
+      'emits states when getWeatherForLatLong is successful',
+      build: () {
+        // Setup the cubit with the mock use cases
+        return HomePageCubit(mockWeatherUseCase, mockCityByNameUseCase);
+      },
+      act: (cubit) async {
+        // Mock the successful response from the use case
+        when(
+          mockWeatherUseCase.getWeatherDataByLatLon(
+            lat: anyNamed('lat'),
+            lon: anyNamed('lon'),
+            units: anyNamed('units'),
+          ),
+        ).thenAnswer((_) {
+          return Future.value(Right(weatherDto));
+        });
+
+        // Call the method you want to test
+        await cubit.getWeatherForLatLong(lat: 0.0, lon: 0.0, units: 'metric');
+      },
+      expect: () => [
+        isA<Loaded>(),
+      ],
+    );
+
+    blocTest(
+      'emits states when getWeatherForLatLong is successful',
+      build: () {
+        // Setup the cubit with the mock use cases
+        return HomePageCubit(mockWeatherUseCase, mockCityByNameUseCase);
+      },
+      act: (cubit) async {
+        // Mock the successful response from the use case
+        when(
+          mockWeatherUseCase.getWeatherDataByLatLon(
+            lat: anyNamed('lat'),
+            lon: anyNamed('lon'),
+            units: anyNamed('units'),
+          ),
+        ).thenAnswer((_) {
+          return Future.value(Right(weatherDto));
+        });
+
+        // Call the method you want to test
+        await cubit.getWeatherForPlace(
+          'name',
+          'secondary',
+          lat.round().toString(),
+          lon.round().toString(),
         );
-      });
-    });
+      },
+      expect: () => [
+        HomePageState.loading(),
+        isA<Loaded>(),
+      ],
+    );
 
-    test('initial state has default value for customProperty', () {
-      final homePageCubit = HomePageCubit();
-      expect(homePageCubit.state, equals(HomePageState.initial()));
-    });
+    blocTest(
+      'emits [Loading, Error] states when getWeatherForLatLong fails',
+      build: () {
+        // Setup the cubit with the mock use cases
+        return HomePageCubit(mockWeatherUseCase, mockCityByNameUseCase);
+      },
+      act: (HomePageCubit cubit) async {
+        // Mock the failure response from the use case
+        when(
+          mockWeatherUseCase.getWeatherDataByLatLon(
+            lat: anyNamed('lat'),
+            lon: anyNamed('lon'),
+            units: anyNamed('units'),
+          ),
+        ).thenAnswer((_) async => Left(ServerFailure(message: 'Error')));
 
-    */
-/*blocTest<HomePageCubit, HomePageState>(
-      'CustomHomePageEvent emits nothing',
-      build: HomePageCubit.new,
-      act: (bloc) => bloc.add(const CustomHomePageEvent()),
-      expect: () => <HomePageState>[],
-    );*//*
-
+        // Call the method you want to test
+        await cubit.getWeatherForLatLong(lat: 0.0, lon: 0.0, units: 'metric');
+      },
+      expect: () => [
+        isA<HomePageError>(),
+      ],
+    );
   });
 }
-*/
